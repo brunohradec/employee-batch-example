ffdb2adea594494dded644d4980da670f30d9054
Bruno Hradec <bhradec@gmail.com>
2024-05-01 23:53:53 +0200

    Update comments

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Employee batch processing example
+
+This is an example application I used to try out Spring Batch.
+
+The app reads accepted job candidates from the database, transforms them into 
+employees and writes them back to the database. After that, the app mocks sending a
+welcome email to each of the new employees.
--- src/main/java/me/bhradec/employeebatchexample/config/BatchConfig.java
+++ src/main/java/me/bhradec/employeebatchexample/config/BatchConfig.java
@@ -17,6 +17,8 @@ import org.springframework.batch.item.data.RepositoryItemReader;
 import org.springframework.batch.item.data.RepositoryItemWriter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
+import org.springframework.core.task.TaskExecutor;
 import org.springframework.data.domain.Sort;
 import org.springframework.transaction.PlatformTransactionManager;
 
@@ -45,13 +47,31 @@ public class BatchConfig {
 
     // Readers:
 
+    /* A bean used to read data about candidates used for batch processing. In this example,
+     * the RepositoryItemReader class is used that reads the data from a provided Spring Data
+     * JPA repository. Spring Batch provides many classes used for reading data for batch
+     * processing, including classes that can be used with JDBC, CSV files and so on. */
     @Bean
     public RepositoryItemReader<Candidate> acceptedCandidateReader() {
         RepositoryItemReader<Candidate> reader = new RepositoryItemReader<>();
 
+        /* The Spring Data JPA repository that will be used to read data from the database
+        is provided to the RepositoryItemReader. */
         reader.setRepository(candidateRepository);
+
+        /* The name of the method that will be used to read data from the database is provided
+         * to the RepositoryItemReader. The method must be defined in the provided repository. */
         reader.setMethodName("findByStatus");
+
+        /* The arguments that will be passed to the method are provided to the RepositoryItemReader. */
         reader.setArguments(List.of(CandidateStatus.ACCEPTED));
+
+        /* Sort must be added or else Spring Batch raises an exception. The sort must
+         * be provided to stabilize the SQL query - batches of data that are being batch
+         * processed are read in chunks of a given size from the database. Without the sort,
+         * there is no guarantee that the ordering of things won't change on every fetch from
+         * the database, causing some records to appear multiple times in different batches,
+         * and some to never appear. */
         reader.setSort(new HashMap<>() {{
             put("id", Sort.Direction.ASC);
         }});
@@ -75,6 +95,10 @@ public class BatchConfig {
 
     // Writers:
 
+    /* A bean used to write data about employees used for batch processing. In this example,
+     * the RepositoryItemWriter class is used that writes the data to a provided Spring Data
+     * JPA repository. Spring Batch provides many classes used for writing data for batch
+     * processing, including classes that can be used with JDBC, CSV files and so on. */
     @Bean
     public RepositoryItemWriter<Employee> employeeWriter() {
         RepositoryItemWriter<Employee> writer = new RepositoryItemWriter<>();
@@ -97,25 +121,42 @@ public class BatchConfig {
         return new SendWelcomeEmailProcessor();
     }
 
+    // Task executors:
+
+    /* A bean used to execute tasks in parallel. In this example, the SimpleAsyncTaskExecutor
+     * class is used that executes tasks in a separate thread. Spring Batch provides many
+     * classes used for executing tasks in parallel, including classes that can be used with
+     * a thread pool, a task scheduler and so on. The TaskExecutor is not necessary. Without
+     * defining the TaskExecutor, batch processing will be executed sequentially without using
+     * multiple threads. */
+    @Bean
+    public TaskExecutor taskExecutor() {
+        SimpleAsyncTaskExecutor executor = new SimpleAsyncTaskExecutor();
+        executor.setConcurrencyLimit(10);
+        return executor;
+    }
+
     // Steps:
 
     @Bean
     public Step importEmployeesStep() {
         return new StepBuilder("importEmployeesStep", jobRepository)
-                .<Candidate, Employee>chunk(10, platformTransactionManager)
+                .<Candidate, Employee>chunk(100, platformTransactionManager)
                 .reader(acceptedCandidateReader())
                 .processor(importEmployeeProcessor())
                 .writer(employeeWriter())
+                .taskExecutor(taskExecutor())
                 .build();
     }
 
     @Bean
     public Step sendWelcomeEmailsStep() {
         return new StepBuilder("sendWelcomeEmailsStep", jobRepository)
-                .<Employee, Employee>chunk(10, platformTransactionManager)
+                .<Employee, Employee>chunk(100, platformTransactionManager)
                 .reader(notEmailedEmployeeReader())
                 .processor(sendWelcomeEmailProcessor())
                 .writer(employeeWriter())
+                .taskExecutor(taskExecutor())
                 .build();
     }
 
